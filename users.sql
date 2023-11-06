82a06025c23c5871a4cb1491a8905d8bffef156e
Brynjardrageide <114694581+Brynjardrageide@users.noreply.github.com>
2023-11-06 11:27:37 +0100

    yay

---
+++ users.sql
@@ -0,0 +1,9 @@
+CREATE TABLE USER (
+    ID INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    email TEXT NOT NULL,
+    mobile TEXT NOT NULL,
+    picture TEXT NOT NULL
+);
